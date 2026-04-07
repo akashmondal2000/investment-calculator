63a03a6b2a8c70dac0c985b99a91dc88d97212e6
akashmondal2000 <mondalakash494@gmail.com>
2026-04-08 04:47:50 +0530

    render the values, conditionally render the result table

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 import Results from "./components/Results";
 
-
 function App() {
-
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration > 0;
 
   function handleChange(inputIdentifire, newValue) {
     setUserInput((prevUserInput) => {
@@ -25,16 +25,17 @@ function App() {
 
   return (
     <>
-      <Header/>
-      <UserInput userInput={userInput} onChange={handleChange}/>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
 
       {/* Results */}
-
-      <Results input={userInput}/>
-     
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please Enter a Duration greather then Zero</p>
+      )}
     </>
-    
-  )
+  );
 }
 
-export default App
+export default App;
